df3abe93f40a511c37c93471aeeb67822ca6f301
ankurdotio <prajapatiankur8349@gmail.com>
2026-01-08 04:11:48 +0530

    add Cli component with terminal commands and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Github from './components/windows/Github'
 import Note from './components/windows/Note'
 import Resume from './components/windows/Resume'
 import Spotify from './components/windows/Spotify'
+import Cli from './components/windows/Cli'
 
 
 
@@ -19,6 +20,7 @@ function App() {
       <Note />
       <Resume />
       <Spotify />
+      <Cli />
     </main>
   )
 }
